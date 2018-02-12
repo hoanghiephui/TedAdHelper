14ec59ffaada11d2a90f3569c328a08e270bff6a
Hoang Hiep <hoanghiep.hui@gmail.com>
2018-02-12 11:29:26 +0700

    update gradle

--- app/src/main/java/gun0912/tedadhelperdemo/MainActivity.java
+++ app/src/main/java/gun0912/tedadhelperdemo/MainActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.facebook.ads.InterstitialAd;
@@ -61,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
         TedAdBanner.showBanner(bannerContainer, FACEBOOK_KEY_BANNER, ADMOB_KEY_BANNER, TedAdHelper.AD_FACEBOOK, new OnBannerAdListener() {
             @Override
             public void onError(String errorMessage) {
-
+                Toast.makeText(MainActivity.this, errorMessage, Toast.LENGTH_SHORT).show();
             }
 
             @Override
